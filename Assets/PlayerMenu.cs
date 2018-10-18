bfd2c6ba770465ce75077e918ac84fd13b8a12e6
Martux99 <34321961+Martux99@users.noreply.github.com>
2018-10-18 15:08:12 -0500

    stupid

---
+++ Assets/PlayerMenu.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMenu : MonoBehaviour {
+
+    public Rigidbody rb;
+
+    public float forwardForce = 200f;
+    public float maxForce = 100f;
+    public float minForce = -100f;
+    int cuenta;
+    bool notouch;
+    public bool direccion = false;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (transform.position.z < 0)
+        {
+            transform.position = new Vector3(-4.51f, 1, 0);
+        }
+        cuenta = Input.touchCount;
+        if (cuenta == 1)
+        {
+            notouch = false;
+            Touch touch = (Input.GetTouch(0));
+            if (Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, 0, -1.73f)).x < 0)
+            {
+                direccion = false;
+            }
+            if (Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, 0, -1.73f)).x > 0)
+            {
+                direccion = true;
+            }
+            if (direccion == false)
+            {
+                if (rb.velocity.z < maxForce)
+                    rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+            }
+            else if (direccion == true)
+            {
+                if (rb.velocity.z > minForce && transform.position.z > 0)
+                {
+                    rb.AddForce(0, 0, -forwardForce * Time.deltaTime);
+                }
+                else if (transform.position.z < 0)
+                {
+                    transform.position = new Vector3(-4.51f, 1, 0);
+                }
+            }
+        }
+        }
+}
